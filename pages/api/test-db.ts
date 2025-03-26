2c3bc6ce81ffef00588dd33ab57ab2e33e852d3d
Ligia <ltatucu@artus.com>
2025-03-26 13:18:58 +0200

    test

--- pages/api/test-db.ts
+++ pages/api/test-db.ts
@@ -1,11 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import prismadb from '@/lib/prismadb';
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
     const users = await prismadb.user.findMany();
     res.status(200).json({ users });
   } catch (error) {
     console.error('[DB ERROR]', error);
-    res.status(500).json({ error: error.message });
+    res
+      .status(500)
+      .json({
+        error: error instanceof Error ? error.message : 'Unknown error',
+      });
   }
 }
